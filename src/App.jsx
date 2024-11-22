c87f32f35bf7287f84e5d4bae1c9f8a0856d1807
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-11-22 17:12:02 +0200

    adds function to fetch blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import './App.css'
+import {useState, useEffect} from 'react'
 
+
+
+const apiUrl ="https://jsonplaceholder.typicode.com/posts"
 function App() {
+  const [ blogData, setBlogData ] = useState([])
+  const [ error, setError ] = useState(null)
   
-  return (
-    <h1>hello world</h1>
-  )
+  useEffect(()=>{
+    fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      setBlogData(data)
+      console.log(blogData)
+    })
+    .catch((error) =>{
+      setError(error)
+    })
+}, [])
+
+if(error) {
+  return <h1>Data fetching failed</h1>}
+
+return( 
+  <div>
+    <h1>title</h1>
+    <p>content</p>
+  </div>
+)
 }
 
 export default App
