496a108fdae46cf43c49c7d384e01da93dca768d
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-11-22 17:45:29 +0200

    Implement error handling and display UI for loading, success, and error states

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,46 @@
-import {useState, useEffect} from 'react'
-
-
+import React from 'react'
+import { useEffect, useState } from 'react'
 
 const apiUrl ="https://jsonplaceholder.typicode.com/posts"
 function App() {
   const [ blogData, setBlogData ] = useState([])
+  const [ loading, setLoading ] = useState(true)
   const [ error, setError ] = useState(null)
   
   useEffect(()=>{
     fetch(apiUrl)
-    .then((res) => res.json())
+    .then((res) => {
+      if(!res.ok){
+        throw new Error('Data fetching failed')
+      }
+      return res.json()
+    })
     .then((data) => {
       setBlogData(data)
-      console.log(blogData)
-    })
-    .catch((error) =>{
-      setError(error)
+      setLoading(false)
+      })
+    .catch(() =>{
+      setError("Data fetching failed")
+      setLoading(false)
     })
 }, [])
 
+if (loading) return <h1>Loading...</h1>
+
 if(error) {
-  return <h1>Data fetching failed</h1>}
+  return <h1>{error}</h1>}
 
 return( 
   <div>
-    <h1>title</h1>
-    <p>content</p>
+    <h1>Posts</h1>
+      <ol>
+        {blogData.map((item)=>( 
+          <li key={item.id}>
+                <h2>{item.title}</h2>
+                <p>{item.body}</p>
+                </li>
+          ))}
+      </ol>
   </div>
 )
 }
